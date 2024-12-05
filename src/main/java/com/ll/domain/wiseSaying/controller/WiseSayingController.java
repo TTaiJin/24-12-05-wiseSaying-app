06dc98266dc5f1cd00f4a434d4fefb4d89f4cf9e
TTaiJin <taeparis980110@gmail.com>
2024-12-05 16:09:59 +0900

    f3 g

--- src/main/java/com/ll/domain/wiseSaying/controller/WiseSayingController.java
+++ src/main/java/com/ll/domain/wiseSaying/controller/WiseSayingController.java
@@ -1,11 +1,17 @@
 package com.ll.domain.wiseSaying.controller;
 
+import com.ll.domain.wiseSaying.entity.WiseSaying;
+import com.ll.domain.wiseSaying.service.WiseSayingService;
+
 import java.util.Scanner;
 
 public class WiseSayingController {
     private final Scanner scanner;
+    private final WiseSayingService wiseSayingService;
+
     public WiseSayingController(Scanner scanner) {
         this.scanner = scanner;
+        this.wiseSayingService = new WiseSayingService();
     }
 
     public void actionAdd() {
@@ -15,5 +21,7 @@ public class WiseSayingController {
         System.out.print("작가 : ");
         String author = scanner.nextLine();
 
+        WiseSaying wiseSaying = wiseSayingService.add(content, author);
+        System.out.println(wiseSaying.getId() + "번 명언이 등록되었습니다.");
     }
 }
