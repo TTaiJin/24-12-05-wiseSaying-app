775e5495fcf5eb8811aa2357694853f4281b43ff
TTaiJin <taeparis980110@gmail.com>
2024-12-05 15:43:32 +0900

    f2 g

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,16 +1,20 @@
 package com.ll;
 
 import com.ll.domain.system.controller.SystemController;
+import com.ll.domain.wiseSaying.controller.WiseSayingController;
 
 import java.util.Scanner;
 
 public class App {
     private final Scanner scanner;
     private final SystemController systemController;
+    private final WiseSayingController wiseSayingController;
+
 
     public App(Scanner scanner) {
         this.scanner = scanner;
         this.systemController = new SystemController();
+        this.wiseSayingController = new WiseSayingController(scanner);
     }
 
     public void run() {
@@ -19,6 +23,8 @@ public class App {
         String cmd = scanner.nextLine();
         if (cmd.equals("종료")) {
             systemController.actionExit();
+        } else if (cmd.equals("등록")) {
+            wiseSayingController.actionAdd();
         }
     }
 }
